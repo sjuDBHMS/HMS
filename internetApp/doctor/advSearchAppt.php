75526cda5f1774528f4dc97201265a308b847350
Unknown <hn657097@sju.edu>
2016-12-05 11:19:44 -0500

    Doctor: Advance Search appointment
    Added advance search where Doctor can search for Previous Appointments with Patient First Name, Last Name , ID or in combination

--- internetApp/doctor/advSearchAppt.php
+++ internetApp/doctor/advSearchAppt.php
@@ -6,6 +6,11 @@ $message="";
 if (isset($_REQUEST['reopen'])) {
 	$EmpID=$_SESSION['doctor'];
 	$ApptID=  $_REQUEST['ApptID'];
+	$EmpID=$_SESSION['doctor'];
+	$PatientID=$_REQUEST['ID'];
+	$fName=$_REQUEST['fName'];
+	$lName=$_REQUEST['lName'];
+
 	$query="UPDATE Appointment SET ApptStatus='$EmpID' WHERE ApptID='$ApptID'";
 	mysql_query($query);
 	echo '<div id="results" >';
@@ -15,7 +20,7 @@ if (isset($_REQUEST['reopen'])) {
 	//header("Location: view.php");
 }
 if (isset($_REQUEST['updateAppointment'])) {
-	echo '<div id="results" >';
+	echo '<div id="results" style="padding-top:5%;">';
     updateAppointment();
     echo '</div>';
     unset($_REQUEST['updateAppointment']);
@@ -28,7 +33,7 @@ function updateAppointment()
 	$ApptID=  $_REQUEST['ApptID'];
 	$query="UPDATE Appointment SET Comments='$Comments' ,Prescription='$Prescription' WHERE ApptID='$ApptID'";
 	mysql_query($query);
-	echo '<h5 style="color:blue;font-family: courier;text-align:center;">Appointment Updated</h2>';
+	echo '<h5 style="color:blue;font-family: courier;text-align:center;">Appointment Updated</h5>';
 	displayResults();
 
 }
@@ -43,20 +48,18 @@ function updateAppointment()
 <title>Welcome - <?php echo $_SESSION['EmpFName']; ?></title>
 <script>
 function validateForm() {
-    var date = document.forms["searchForm"]["date"].value;
-    var month = document.forms["searchForm"]["month"].value;
-    var year = document.forms["searchForm"]["year"].value;
-    //alert("month ="+month+" \t date="+date+" month!=00"+(date!="00" && month== "00"));
-    if(date!="00" && month== "00"){
-        document.getElementById("monthError").innerHTML = "Select month to search";
-        document.getElementById("month").focus();
+    var ID = document.forms["searchForm"]["ID"].value;
+    var fName = document.forms["searchForm"]["fName"].value;
+    var lName = document.forms["searchForm"]["lName"].value;
+    if( ID== "" && fName=="" && lName==""){
+        document.getElementById("sumbitError").innerHTML = "Enter some value to search";
+        document.getElementById("ID").focus();
         return false;
     }
 	
 }
 </script>
 </head>
-
 <body bgcolor="#f2f2f2">
 
 <div id="resHeader">
@@ -75,50 +78,22 @@ function validateForm() {
 
 <div id="wrapper">
 <div id="form">
-</br> <label><h2>Search Appointments :</h2></label> </br>
-<form name="searchForm" enctype="multipart/form-data" onsubmit="return validateForm()"  method="post" action="searchAppt.php">
-<label>Month :&emsp;</label>
-	<select name="month" id="month" size="1">
-	<option value="00">--Month--</option>
-    <option value="01">January</option>
-    <option value="02">February</option>
-    <option value="03">March</option>
-    <option value="04">April</option>
-    <option value="05">May</option>
-    <option value="06">June</option>
-    <option value="07">July</option>
-    <option value="08">August</option>
-    <option value="09">September</option>
-    <option value="10">October</option>
-    <option value="11">November</option>
-    <option value="12">December</option>
-	</select><span id="IDError"></span><span id="monthError"></span><br><br>
-	
-<label>Date :&emsp;&emsp;</label>
-	<select name="date" id="date" >
-		<option value="00">--Date--</option>
-		<?for($i=1;$i<=31;$i++):?>
-         <option value="<?=str_pad($i,2,'0',STR_PAD_LEFT)?>"><?=$i?></option>
-    <?endfor?>
-	</select></span><span id="dateError"></span><br><br>
+</br> <label><h2>Advance Search Appointments :</h2></label> </br>
+<form name="searchForm" enctype="multipart/form-data" onsubmit="return validateForm()"  method="post" action="advSearchAppt.php">
 
-<label>Year :&emsp;&emsp;</label>
-	<select name="year" id="year" ></select></span><span id="yearError"></span><br><br>
-	
-<input type="submit" name="search" value="Search"><a style="padding-left:20px;text-decoration: none" href="advSearchAppt.php">Advanced Search</a></br>
+<label>Patient ID:&emsp;</label>
+	<input type="text" name="ID" id="ID" placeholder="Login ID"><span id="IDError"></span><br><br>
 
-<script>
-	var end = 1900;
-	var start = new Date().getFullYear();
-	var options = "";
-	for(var year = start ; year >=end; year--){
-	  options += "<option>"+ year +"</option>";
-	}
-	document.getElementById("year").innerHTML = options;
-</script>
+
+<label>First Name:&ensp;</label>
+	<input type="text" name="fName" id="fName" placeholder="First Name"><span id="fNameError"></span><br><br>
+
+	<label>Last Name:&ensp;&thinsp;</label>
+	<input type="text" name="lName" id="lName" placeholder="Last Name"><span id="fNameError"></span><br><br>	
+<input type="submit" name="search" value="Search"><a style="padding-left:20px;text-decoration: none" href="searchAppt.php">Search by Date</a></br>&emsp;<span id="sumbitError"></span>
 </form>
 </div>
-<div id="results">
+<div id="results"  style="padding-top:5%;">
 <?php
 if (isset($_REQUEST['search'])) {
 		displayResults();
@@ -129,42 +104,28 @@ if (isset($_REQUEST['search'])) {
 <?php function displayResults() {?>
 <?php 
 $EmpID=$_SESSION['doctor'];
-$date=$_REQUEST['date'];
-$month=$_REQUEST['month'];
-$year=$_REQUEST['year'];
-if($month=="00"){
-	$messageDate=$year;
-	$apptQuery="SELECT a.* 
-			FROM appointment a  INNER JOIN seenby s
-			ON a.ApptID=s.ApptID
-			Where a.ApptStatus='closed' And s.EmpId='$EmpID' AND YEAR(a.ApptDate)='$year'
-			ORDER BY a.ApptDate DESC";
+$PatientID=$_REQUEST['ID'];
+$fName=$_REQUEST['fName'];
+$lName=$_REQUEST['lName'];
 
-}else if($month!="00" && $date!="00"){
-$dateObj   = DateTime::createFromFormat('!m', $month);
-$monthName = $dateObj->format('F'); 
-$messageDate=$monthName.", ".$date.", ".$year;
-$fullDate="$year-$month-$date";
-$apptQuery="SELECT a.* 
-			FROM appointment a  INNER JOIN seenby s
+	if($fName!='' &&$lName=='' && $PatientID=='')
+		$message="First Name: $fName";
+	else if($fName=='' &&$lName!='' && $PatientID=='')
+		$message=" Last Name: $lName ";
+	else if($fName=='' &&$lName=='' && $PatientID!='')
+		$message="PatientID: $PatientID";
+	else
+		$message="PatientID:'$PatientID', First Name:'$fName', Last Name:'$lName', ";
+	$apptQuery="SELECT a.* 
+			FROM (SELECT app.* FROM appointment app INNER JOIN patient p ON app.PatientID= p.PatientID WHERE (p.PatientID='$PatientID' or p.PatientFName='$fName' or p.PatientLName='$lName')) a  INNER JOIN seenby s
 			ON a.ApptID=s.ApptID
-			Where a.ApptStatus='closed' And s.EmpId='$EmpID' AND a.ApptDate='$fullDate'
+			Where a.ApptStatus='closed' And s.EmpId='$EmpID'
 			ORDER BY a.ApptDate DESC";
-}else{
-$dateObj   = DateTime::createFromFormat('!m', $month);
-$monthName = $dateObj->format('F'); 
-$messageDate=$monthName.", ".$year;
-$apptQuery="SELECT a.* 
-			FROM appointment a  INNER JOIN seenby s
-			ON a.ApptID=s.ApptID
-			Where a.ApptStatus='closed' And s.EmpId='$EmpID' AND YEAR(a.ApptDate)='$year' AND MONTH(a.ApptDate)='$month'
-			ORDER BY a.ApptDate DESC";
-}
 $apptResult = mysql_query($apptQuery);
 //echo mysql_num_rows($result)
 if(mysql_num_rows($apptResult)>0)
 {
-echo '<h2 style="color:green;font-family: courier;text-align:center;">Appointments on '.$messageDate.'.</h2>';
+echo '<h2 style="color:green;font-family: courier;text-align:center;">Appointments on '.$message.'.</h2>';
 ?>
 <div class="listHeadign" style="margin-top:50px; width:90%" ><h2  style="color:#f2f2f2;">&nbsp;Previous Appointments</h2></div>
 <div class="wrapper" id="ApptView" style=" width:90%">
@@ -200,12 +161,12 @@ echo '<h2 style="color:green;font-family: courier;text-align:center;">Appointmen
         	echo '<td>' . $row['ApptDate'] . '</td>';
         	?>
         	<td>
-        		<form method="post" action="searchAppt.php">
+        		<form method="post" action="advSearchAppt.php">
 	        		<textarea rows="4" cols="20" name="Prescription"><?php echo $row['Prescription']; ?></textarea>
 	        		<input type="hidden" name="ApptID" value= <?php echo $row['ApptID']; ?> >
-	        		<input type="hidden" name="date" value= "<?php echo $date; ?>" >
-	        		<input type="hidden" name="month" value= "<?php echo $month; ?>" >
-	        		<input type="hidden" name="year" value= "<?php echo $year; ?>" >
+	        		<input type="hidden" name="ID" value= <?php echo $PatientID; ?> >
+	        		<input type="hidden" name="fName" value= "<?php echo $fName; ?>" >
+	        		<input type="hidden" name="lName" value= "<?php echo $lName; ?>" >
 	        		
 			</td>
         	<td>
@@ -216,11 +177,11 @@ echo '<h2 style="color:green;font-family: courier;text-align:center;">Appointmen
 				</form>
         	</td>
         	<td>
-        	<form method="post" action="searchAppt.php">
+        	<form method="post" action="advSearchAppt.php">
 	        		<input type="hidden" name="ApptID" value= <?php echo $row['ApptID']; ?> >
-	        		<input type="hidden" name="date" value= "<?php echo $date; ?>" >
-	        		<input type="hidden" name="month" value= "<?php echo $month; ?>" >
-	        		<input type="hidden" name="year" value= "<?php echo $year; ?>" >
+	        		<input type="hidden" name="ID" value= <?php echo $PatientID; ?> >
+	        		<input type="hidden" name="fName" value= "<?php echo $fName; ?>" >
+	        		<input type="hidden" name="lName" value= "<?php echo $lName; ?>" >
 	        		<input type="submit" value= "ReOpen" name="reopen"  onclick="return confirm('Are you sure want to reopen Appointmen: <?php echo $row['ApptID']?> ?')" />
 				</form>
         	</td>
@@ -231,7 +192,7 @@ echo '<h2 style="color:green;font-family: courier;text-align:center;">Appointmen
 </table>
 <?php 
 }else
-echo '<h2 style="color:#a51313;font-family: courier;text-align:center;margin-top:50px">No Appointments Found<h2>';
+echo '<h2 style="color:#a51313;font-family: courier;text-align:center;margin-top:50px;">No Appointments Found for search <h3 style="color:gray;font-family: courier;text-align:center;">'.$message.'</h3></h2>';
 ?>
 </div>
 <?php } ?>
